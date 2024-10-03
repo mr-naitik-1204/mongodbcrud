495df900fabef77825d135e67762a5d006923fbb
naitik <naitikkherala47@gmail.com>
2024-10-03 08:46:08 +0530

    first commit

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
 const express = require('express');
 const app = express()
 
-const bodyparser=require('body-parser');
-app.use(bodyparser.urlencoded({extended:true}))
+const bodyparser = require('body-parser');
+const { ObjectId } = require('mongodb');
+app.use(bodyparser.urlencoded({ extended: true }))
 
-const MongoClient=require('mongodb').MongoClient
-const url="mongodb://localhost:27017"
-const client=new MongoClient(url)
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url)
 
 const db = client.db("naitikkherala")
-const collection =db.collection("register")
+const collection = db.collection("register")
 
 client.connect(url)
 
-.then((res)=>{
+    .then((res) => {
         console.log("conect");
-        
+
+    })
+    .catch((eror) => {
+        console.log(eror);
+
+    })
+var editdata = null;
+var id = null;
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+    const data = await collection.find().toArray();
+    res.render('mogo', { data, editdata });
+
 })
-.catch((eror)=>{
-    console.log(eror);
-    
-})
 
-app.set('view engine','ejs')
-
-app.get('/',async(req,res)=>{
-    const data =await collection.find().toArray()
-    console.log(data);
-
-    res.render('mogo',{data})
-    
-})
-
-app.post('/createdata',async(req,res)=>{
+app.post('/createdata', async (req, res) => {
     console.log(req.body);
-    await collection.insertOne(req.body)
-
+    if (id) {
+        await collection.updateOne({ _id: new ObjectId(id) }, { $set: req.body });
+       id = null; 
+        editdata = null; 
+    } else {
+        await collection.insertOne(req.body);
+    }
     res.redirect('/')
-    
+
+})
+
+app.get('/deletedata', async (req, res) => {
+    id = req.query.delet;
+        await collection.deleteOne({ _id: new ObjectId(id) });
+       id = null;
+        res.redirect('/');
+})
+
+app.get('/updetedata', async (req, res) => {
+   id = req.query.up;
+    editdata = await collection.findOne({ _id: new ObjectId(id) });
+    res.redirect('/');
 })
 
 app.listen(8090)
